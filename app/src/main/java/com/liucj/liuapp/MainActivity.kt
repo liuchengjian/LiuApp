a06ca31a4fa623993828582951060004711db161
dqgb2 <627107345@qq.com>
2021-01-06 17:22:36 +0800

    增加必填项

--- app/src/main/java/com/liucj/liuapp/MainActivity.kt
+++ app/src/main/java/com/liucj/liuapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.liucj.liuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.liucj.lib_picture_selector.PictureView
 import com.liucj.liu_ui.tab.bottom.LiuTabBottom
 import com.liucj.liu_ui.tab.bottom.LiuTabBottomInfo
 
@@ -9,15 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tabBottom = findViewById<LiuTabBottom>(R.id.mLiuTabBottom)
-        val homeInfo = LiuTabBottomInfo(
-            "首页",
-            "fonts/iconfont.ttf",
-            getString(R.string.if_home),
-            null,
-            "#ff000000",
-            "#ffd44949"
-        )
-        tabBottom.setLiuTabInfo(homeInfo)
+
+        val mPictureView = findViewById<PictureView>(R.id.mPictureView)
+        mPictureView.setAspectRatio(1,1)
+        mPictureView.init()
     }
 }
